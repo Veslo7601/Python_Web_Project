0aa06d0b323282daba6b3d39aa7ab006c41e8764
Veslo7601 <igrok7601@gmail.com>
2024-06-21 20:44:49 +0300

    modified:   PhotoShare/database/database.py

--- PhotoShare/database/database.py
+++ PhotoShare/database/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from os import environ
 
-DATABASE_URL = environ.get("DATABASE_URL")
+DATABASE_URL = "postgresql+psycopg2://postgres:postgres@localhost:5432/postgres"
+#DATABASE_URL = environ.get("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
