85fd194890dd100d25896a4991e47a7f470bca54
Veslo7601 <igrok7601@gmail.com>
2024-06-21 21:32:24 +0300

    modified:   PhotoShare/database/__pycache__/models.cpython-312.pyc 	modified:   PhotoShare/database/models.py 	modified:   migration/__pycache__/env.cpython-312.pyc 	new file:   migration/versions/6d61ab5ebe44_version_0_1_2.py 	new file:   migration/versions/__pycache__/6d61ab5ebe44_version_0_1_2.cpython-312.pyc 	new file:   migration/versions/__pycache__/a3f2f26389e8_version_0_1_3.cpython-312.pyc 	new file:   migration/versions/__pycache__/d397c7bbb457_version_2.cpython-312.pyc 	new file:   migration/versions/a3f2f26389e8_version_0_1_3.py 	new file:   migration/versions/d397c7bbb457_version_2.py

--- PhotoShare/database/models.py
+++ PhotoShare/database/models.py
@@ -6,13 +6,28 @@ Base = declarative_base()
 class Tags(Base):
     __tablename__ = "tags"
     id = Column(Integer, primary_key=True)
-    tag = Column(String(32), nullable=True)
+    tag = Column(String(32), nullable=True, unique=True)
     images_id = Column(Integer, ForeignKey("images.id"))
     images = relationship("Images", back_populates="tags")
+
+class Comments(Base):
+    __tablename__ = "comments"
+    id = Column(Integer, primary_key=True)
+    comment = Column(String(512), nullable=True)
+    created_at = Column(Date, nullable=True)
+    updated_at = Column(Date, nullable=True)
+    images_id = Column(Integer, ForeignKey("images.id"))
+    images = relationship("Images", back_populates="comments")
 
 class Images(Base):
     __tablename__ = "images"
     id = Column(Integer, primary_key=True)
-    image = Column(String(255), nullable=False)
+    image = Column(String(255), nullable=True)
     created_at = Column(Date, nullable=True)
-    tag = relationship("Tags", back_populates="images", cascade="all, delete-orphan")
+    tags = relationship("Tags", back_populates="images", cascade="all, delete-orphan")
+    comments = relationship("Comments", back_populates="images", cascade="all, delete-orphan")
+
+class Roles(Base):
+    __tablename__ = "roles"
+    id = Column(Integer, primary_key=True)
+    role = Column(String(64), nullable=True, unique=True)
