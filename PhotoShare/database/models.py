a81170f53adaa33252f92626d9e07039b4dc3f59
dmitriy <kylishovdimka@ukr.net>
2024-06-23 19:37:44 +0300

    Init role

--- PhotoShare/database/models.py
+++ PhotoShare/database/models.py
@@ -1,7 +1,13 @@
+import enum
+from datetime import date
+from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.orm import declarative_base, relationship
-from sqlalchemy import Column, Integer, String, ForeignKey, Date, Boolean, func
+from sqlalchemy import Column, Integer, String, ForeignKey, Date, Boolean, func, Enum
 from sqlalchemy.sql.sqltypes import DateTime
+
+
 Base = declarative_base()
+
 
 class Tags(Base):
     __tablename__ = "tags"
@@ -9,6 +15,7 @@ class Tags(Base):
     tag = Column(String(32), nullable=True, unique=True)
     images_id = Column(Integer, ForeignKey("images.id"))
     images = relationship("Images", back_populates="tags")
+
 
 class Comments(Base):
     __tablename__ = "comments"
@@ -19,6 +26,7 @@ class Comments(Base):
     images_id = Column(Integer, ForeignKey("images.id"))
     images = relationship("Images", back_populates="comments")
 
+
 class Images(Base):
     __tablename__ = "images"
     id = Column(Integer, primary_key=True)
@@ -27,17 +35,22 @@ class Images(Base):
     tags = relationship("Tags", back_populates="images", cascade="all, delete-orphan")
     comments = relationship("Comments", back_populates="images", cascade="all, delete-orphan")
 
-class Roles(Base):
-    __tablename__ = "roles"
-    id = Column(Integer, primary_key=True)
-    role = Column(String(64), nullable=True, unique=True)
+
+class Role(enum.Enum):
+    admin: str = "admin"
+    moderator: str = "moderator"
+    user: str = "user"
+
 
 class User(Base):
     __tablename__ = "users"
-    id = Column(Integer, primary_key=True)
-    username = Column(String(50))
-    email = Column(String(250), nullable=False, unique=True)
-    password = Column(String(255), nullable=False)
-    created_at = Column('crated_at', DateTime, default=func.now())
-    refresh_token = Column(String(255), nullable=True)
-    confirmed = Column(Boolean, default=False)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(50))
+    password: Mapped[str] = mapped_column(String(255), nullable=False)
+    email: Mapped[str] = mapped_column(String(75), nullable=False, unique=True)
+    refresh_token: Mapped[str] = mapped_column(String(255), nullable=True)
+    created_at: Mapped[date] = mapped_column("created_at", DateTime, default=func.now())
+    updated_at: Mapped[date] = mapped_column("updated_at", DateTime, default=func.now(), onupdate=func.now())
+    role: Mapped[Enum] = mapped_column("role", Enum(Role), default=Role.user, nullable=True)
+    confirmed: Mapped[bool] = mapped_column(Boolean, default=False, nullable=True)
+    avatar: Mapped[str] = mapped_column(String(255), nullable=True)
