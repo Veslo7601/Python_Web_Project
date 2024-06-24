fb41c71a69d8899fcad76f0196e8c63bdc4cc950
dmitriy <kylishovdimka@ukr.net>
2024-06-24 15:58:48 +0300

    Update requirements.txt

--- PhotoShare/main.py
+++ PhotoShare/main.py
@@ -1,9 +1,12 @@
 import uvicorn
 import redis.asyncio as redis
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi_limiter import FastAPILimiter
+from sqlalchemy import text
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from PhotoShare.database.database import get_database
 from PhotoShare.routers import auth, users
 from PhotoShare.conf.config import settings
 
@@ -28,9 +31,26 @@ async def startup():
     r = await redis.Redis(host=settings.redis_host, port=settings.redis_port, db=0)
     await FastAPILimiter.init(r)
 
-    # return {
-    #     "status": "ok"
-    # }
+
+@app.get("/")
+def index():
+    return {"message": "Hello world"}
+
+
+@app.get("/api/healthchecker")
+async def healthchecker(db: AsyncSession = Depends(get_database)):
+    try:
+        # Make request
+        result = await db.execute(text("SELECT 1"))
+        result = result.fetchone()
+        if result is None:
+            raise HTTPException(
+                status_code=500, detail="Database is not configured correctly"
+            )
+        return {"message": "Welcome to FastAPI!"}
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500, detail="Error connecting to the database")
 
 
 if __name__ == "__main__":
